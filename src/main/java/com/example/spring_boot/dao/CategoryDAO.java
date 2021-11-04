6b93a7402c7f1b5068187f839f707cfbd01e7f17
d3m0nhunt3r <d3m0nhunt3r92@gmail.com>
2021-11-04 19:55:27 +0200

    clear branch

--- src/main/java/com/example/spring_boot/dao/CategoryDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_boot.dao;
-
-import com.example.spring_boot.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDAO extends JpaRepository<Category, Integer> {
-}
--- src/main/java/com/example/spring_boot/dao/ProductDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_boot.dao;
-
-import com.example.spring_boot.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDAO extends JpaRepository<Product, Integer> {
-}
