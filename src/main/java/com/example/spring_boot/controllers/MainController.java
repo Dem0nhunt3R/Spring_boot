b3c7e6aefc902155a3dfc2ee0ae429b9a331aff1
d3m0nhunt3r <d3m0nhunt3r92@gmail.com>
2021-11-04 19:52:32 +0200

    spring boot homework 3

--- src/main/java/com/example/spring_boot/controllers/MainController.java
+++ src/main/java/com/example/spring_boot/controllers/MainController.java
@@ -1,15 +1,33 @@
 package com.example.spring_boot.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_boot.models.Product;
+import com.example.spring_boot.services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class MainController {
 
-    @GetMapping("/")
-    public String home(){
-        return "hello";
+    private ProductService productService;
+    
+    @PostMapping("/product")
+    public List<Product> saveProduct(@RequestBody Product product){
+        return productService.saveProductWithCategory(product);
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable int id){
+        System.out.println("get user");
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public List<Product> deleteProduct(@PathVariable int id){
+        return productService.deleteProductById(id);
     }
 
 }
-
